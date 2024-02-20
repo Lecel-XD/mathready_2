44a065fa9922398daf6c22145e70ee8e0d769992
Lecel <138223658+Lecel-XD@users.noreply.github.com>
2024-02-20 13:20:29 +0500

    v.0.0.2.2

--- ten-eleven_numbers.php
+++ ten-eleven_numbers.php
@@ -15,7 +15,33 @@
         <div class="theme_box">
             <div class="theme_text">Виды чисел</div>
         </div>
-        <div class="numbers_box"></div>
+        <div class="numbers_box">
+            <div class="number_box">
+                <p>Натуральные числа - используются при счёте предметов</p>
+                <p>N = {1, 2, 3, ...}</p>
+            </div>
+            <div class="number_box">
+                <p>Целые числа - натуральные числа, отрицательные и нуль</p>
+                <p>Z = {..., -2, -1, 0, 1, 2, ...}</p>
+            </div>
+            <div class="number_box">
+                <p>Рациональные числа - обыкновенные дроби</p>
+                <p></p>
+                <p></p>
+            </div>
+            <div class="number_box">
+                <p>Иррациональные числа</p>
+                <p></p>
+            </div>
+            <div class="number_box">
+                <p>Действительные числа</p>
+                <p></p>
+            </div>
+            <div class="number_box">
+                <p>Комплексные числа</p>
+                <p></p>
+            </div>
+        </div>
 
     </div>
 </body>
